01e0b7dc63889a7f8364f291c0e104ea59324145
Haley Visconti <hvisconti1343@outlook.com>
2024-10-14 16:10:27 +0100

    finished task 5

--- models/models.js
+++ models/models.js
@@ -16,3 +16,12 @@ exports.fetchArticleById = (id) => {
         return rows[0]
     })
 }
+
+exports.fetchArticles = () => {
+    return db.query(`SELECT article_id, title, topic, author, created_at, votes, article_img_url,
+        CAST((SELECT COUNT(*) FROM comments WHERE comments.article_id = articles.article_id) AS INT) AS comment_count
+        FROM articles ORDER BY created_at DESC;`)
+    .then((result) => {
+        return result.rows;
+    })
+}
