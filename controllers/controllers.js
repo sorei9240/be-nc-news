ef608569b03d1ea65e56994055dad66c46b39f2d
Haley Visconti <hvisconti1343@outlook.com>
2024-10-18 14:30:03 +0100

    completed task 20 add pagination to GET articles endpoint

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -34,10 +34,10 @@ exports.getArticleById = (req, res, next) => {
 }
 
 exports.getArticles = (req, res, next) => {
-    const { sort_by, order, topic } = req.query;
-    return fetchArticles(sort_by, order, topic)
-    .then((articles) => {
-        res.status(200).send({ articles })
+    const { sort_by, order, topic, limit, p } = req.query;
+    return fetchArticles(sort_by, order, topic, limit, p)
+    .then(({ articles, totalCount }) => {
+        res.status(200).send({ articles, totalCount })
     }).catch((err) => {
         next(err);
     })
